c053cc2b088311ece01195bb8dc896e89a6d3321
Moisés Mariano <moisesduartem@gmail.com>
2021-01-04 22:39:20 -0300

    add UsersController.store to routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,6 @@ import UsersController from './controllers/UsersController'
 const routes = Router()
 
 routes.get('/users', UsersController.index)
+routes.post('/users', UsersController.store)
 
 export default routes
