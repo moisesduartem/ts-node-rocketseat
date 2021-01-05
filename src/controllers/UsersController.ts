fd400af62c784d430a0179679f42d30aa1acb9db
Moisés Mariano <moisesduartem@gmail.com>
2021-01-04 22:38:34 -0300

    create a store method

--- src/controllers/UsersController.ts
+++ src/controllers/UsersController.ts
@@ -6,6 +6,11 @@ class UsersController {
     const users = await User.find()
     return res.json(users)
   }
+
+  public async store (req: Request, res: Response): Promise<Response> {
+    const user = await User.create(req.body)
+    return res.json(user)
+  }
 }
 
 export default new UsersController()
